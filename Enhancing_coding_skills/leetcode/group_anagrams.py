f2ae4f042ff13f53680c73f404d2ed4fb4550a9b
irangelova <ivetarangelova@gmail.com>
2025-01-14 16:30:56 +0100

    Solutions to additional tasks for enhancing coding skills

--- Enhancing_coding_skills/leetcode/group_anagrams.py
+++ Enhancing_coding_skills/leetcode/group_anagrams.py
@@ -1,18 +1,14 @@
 from collections import defaultdict
 
-class Solution(object):
-    def groupAnagrams(self, strs: list) -> list:
-        anagram_map = defaultdict(list)
-        result = []
+strings = input().split(", ")
+anagram_map = defaultdict(list)
+result = []
 
-        for element in strs:
-            sorted_element = tuple(sorted(element))
-            anagram_map[sorted_element].append(element)
+for element in strings:
+    sorted_element = tuple(sorted(element))
+    anagram_map[sorted_element].append(element)
 
-        for value in anagram_map.values():
-            result.append(value)
+for value in anagram_map.values():
+    result.append(value)
 
-        return result
-
-    strings = input()
-    groupAnagrams(strings)
+print(result)
